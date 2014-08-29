3779d67166e8707f03a57c56681f70eebdb3d5c7
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-08-29 23:40:25 +0200

    add test coverage with coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ Bundler.require :default, Sinatra::Application.environment
 require 'rspec'
 require File.dirname(__FILE__) + '/../web.rb'
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   
 end
